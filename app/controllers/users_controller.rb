dbd24ef92624b73ecc9f807a8e664fb18c6cfcd5
MarKoz162 <88556770+MarKoz162@users.noreply.github.com>
2021-12-02 12:39:45 +0100

    Add_routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  skip_before_action :authenticate_user!, only: [:index]
   def index
     render json: { data: ActiveModel::SerializableResource.new(User.all, each_serializer: UserSerializer) }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :stocks
+  resources :user_stocks, only: [:create, :index] 
+  post "sell", to: "user_stocks#sell"
+  resources :users, only: [:index]
   root 'stocks#index'
-  get "All_users", to: "users#index"
   patch "donate", to: "users#donate"
 end
