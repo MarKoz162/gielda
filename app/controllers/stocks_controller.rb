a449fda26ce6f1060ac09562d200d2b60e5b858f
MarKoz162 <88556770+MarKoz162@users.noreply.github.com>
2021-12-02 12:38:10 +0100

    Add_user_stock_create_and_sell_method

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,27 +1,23 @@
 class StocksController < ApplicationController
   before_action :set_stock, only: %i[ show edit update destroy ]
-
-  # GET /stocks or /stocks.json
+  skip_before_action :authenticate_user!, only: [:index,:create]
+  
   def index
     render json: { data: ActiveModel::SerializableResource.new(Stock.all, each_serializer: StockSerializer) }
   end
 
-  # GET /stocks/1 or /stocks/1.json
   def show
     render json: StockSerializer.new(@stock).to_h
   end
 
-  # GET /stocks/new
   def new
     @stock = Stock.new
   end
 
-  # GET /stocks/1/edit
   def edit
     
   end
 
-  # POST /stocks or /stocks.json
   def create
     @stock = Stock.new(stock_params)
     @stock.sold_price = 0.8 * @stock.purchase_price
@@ -33,7 +29,6 @@ class StocksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /stocks/1 or /stocks/1.json
   def update
     if @stock.update(stock_params)
         render json: @stock
@@ -42,7 +37,6 @@ class StocksController < ApplicationController
     end
   end
 
-  # DELETE /stocks/1 or /stocks/1.json
   def destroy
     if @stock.destroy
       render json: @stock
@@ -52,12 +46,10 @@ class StocksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_stock
       @stock = Stock.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def stock_params
       params.require(:stock).permit(:name, :quantity, :purchase_price)
     end
--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,15 +1,76 @@
 class UserStocksController < ApplicationController
-  
+  before_action :set_stock, only: [:create, :sell]
+  skip_before_action :authenticate_user!, only: [:index]
+  def index
+    render json: { data: ActiveModel::SerializableResource.new(UserStock.all, each_serializer: UserStockSerializer) }
+  end
 
-  def donate
-    @user = User.find(params[:id])
-    @user.money = @user.money + params[:money]
-    if @user.save
-      render json: @user
+
+  def create
+    if @stock.quantity >= params[:quantity]
+      if current_user.money >= params[:quantity] * @stock.purchase_price
+        if UserStock.where(user_id: current_user.id, stock_id: params[:stock_id]).empty?
+          @user_stocks = UserStock.create(user: current_user, stock: @stock, quantity: params[:quantity])
+          if @user_stocks.save
+            calculate_money_and_quantity(params[:quantity])
+          else
+            render json: { errors: @user_stocks.errors.messages }, status: :unprocessable_entity
+          end 
+        else
+          @user_stocks = UserStock.find_by(user_id: current_user, stock_id: @stock.id) 
+          if @user_stocks.update(quantity: @user_stocks.quantity + params[:quantity])
+            calculate_money_and_quantity(params[:quantity])
+          else
+            render json: { errors: @user_stocks.errors.messages }, status: :unprocessable_entity
+          end
+        end
+      else
+        render json: { errors: "You don't have enough money" }, status: :unprocessable_entity
+      end  
     else
-      render json: { errors: @user.errors.messages }, status: :unprocessable_entity
+      render json: { errors: "We don't have enough stock to sell " }, status: :unprocessable_entity
     end
   end
 
+
+  def sell
+    if UserStock.where(user_id: current_user.id, stock_id: params[:stock_id]).empty?
+      render json: { errors: "You didn't buy any stock with such an id" }, status: :unprocessable_entity
+    else
+      @user_stock = UserStock.find_by(user_id: current_user, stock_id: @stock.id) 
+      if @user_stock.quantity >= params[:quantity]
+        if @user_stock.update(quantity: @user_stock.quantity - params[:quantity])
+          add_money_and_quantity(-params[:quantity])
+          if @user_stock.quantity == 0
+            @user_stocks.destroy
+            render json: @user_stock
+          else
+            render json: @user_stock
+          end
+        else
+          render json: { errors: @user_stock.errors.messages }, status: :unprocessable_entity
+        end
+      else
+        render json: { errors: "You don't own that many stocks" }, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private
+
+  def set_stock
+    @stock = Stock.find(params[:stock_id])
+  end
+
+  def subtract_money_and_quantity(quantity)
+    current_user.calculate_money(-quantity * @stock.purchase_price)
+    @stock.calculate_quantity(-quantity)
+    render json: @user_stocks
+  end
+
+  def add_money_and_quantity(quantity)
+    current_user.calculate_money(-quantity * @stock.sold_price)
+    @stock.calculate_quantity(-quantity)
+  end
 
 end
