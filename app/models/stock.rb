33d853f879a25fdc76a0155dd7c8d65ef46382bd
MarKoz162 <88556770+MarKoz162@users.noreply.github.com>
2022-01-03 12:38:46 +0100

    Add_validation_unique_name

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
     validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
     validates :purchase_price, presence: true
     validates :sold_price, presence: true
